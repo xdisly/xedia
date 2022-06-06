233a9f79ff00727e30df3a49391678150f0eb035
xDisly <razigraevvlad@gmail.com>
2022-06-06 18:38:32 +1000

    project page edit

--- resources/views/home/projects.blade.php
+++ resources/views/home/projects.blade.php
@@ -37,7 +37,6 @@
                             <div class="project__social">
                                 <a href="https://topsoundboard.ru" class="other"><i class="fa fa-globe-americas"></i></a>
                                 <a href="https://vk.com/topsoundboard" class="vk"><i class="fa fa-vk"></i></a>
-                                <a href="https://instagram.com/topsoundboard" class="instagram"><i class="fa fa-instagram"></i></a>
                                 <a href="https://t.me/topsoundboard" class="telegram"><i class="fab fa-telegram-plane"></i></a>
                             </div>
                         </div>
@@ -89,7 +88,7 @@
                         <div class="project__text">
                             <h5>6X6</h5>
                             <span>Платформа tvcube▢</span>
-                            <p>Развлекательный канал для молодой аудитории</p>
+                            <p>Развлекательный канал</p>
                             <div class="project__social">
                                 <a href="https://tvcube.ru" class="other"><i class="fa fa-globe-americas"></i></a>
                                 <a href="https://twitch.tv/6x6tv" class="twitch"><i class="fa fa-twitch"></i></a>
@@ -105,15 +104,15 @@
                 <div class="col-lg-12">
                     <div class="project__item">
                         <div class="project__pic">
-                            <img src="{{ asset ('assets/front/img/banner/qchannel_banner.png') }}" alt="">
+                            <img src="{{ asset ('assets/front/img/banner/neotv_banner.png') }}" alt="">
                         </div>
                         <div class="project__text">
-                            <h5>Q Channel</h5>
+                            <h5>NEO TV</h5>
                             <span>Платформа tvcube▢</span>
-                            <p>Канал развлекательной тематики</p>
+                            <p>Развлекательно-информационный канал</p>
                             <div class="project__social">
                                 <a href="https://tvcube.ru" class="other"><i class="fa fa-globe-americas"></i></a>
-                                <a href="https://catcast.tv/q_channel" class="twitch"><i class="fa fa-play"></i></a>
+                                <a href="https://catcast.tv/neo_tv" class="twitch"><i class="fa fa-play"></i></a>
                             </div>
                         </div>
                     </div>
